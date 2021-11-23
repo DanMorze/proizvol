7ede3cdb79e85eff9a74564dbd1458e2572357eb
Danil <brazhkinDANI@yandex.ru>
2021-11-23 22:44:56 +0300

    Revert "AllColorAndPy"
    This reverts commit ef39bc1f

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import sys
 
 from random import randint
+from PyQt5 import uic
 from PyQt5.QtGui import QColor, QPainter
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from UI import Ui_MainWindow
 
 
 SCREEN_SIZE = [800, 600]
 
 
-class RandomCircle(QMainWindow, Ui_MainWindow):
+class RandomCircle(QMainWindow):
     def __init__(self):
-        super().__init__()
-        self.setupUi(self)
+        super(QMainWindow, self).__init__()
+        uic.loadUi("UI.ui", self)
         self.setFixedSize(*SCREEN_SIZE)
         self.do_paint = False
         self.click = 0
